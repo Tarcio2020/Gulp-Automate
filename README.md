d51c99188fafda879c5b3c1f13db250a68cf41b7
Tarcio Teles <tarcioapps7@gmail.com>
2023-06-18 06:50:57 -0300

    add

--- README.md
+++ README.md
@@ -34,3 +34,47 @@ Vamos na pasta >> package.js <<
 depois comando
 npm run gulp
 
+
+Exportar os pacotes Gulp
+const gulp = require('gulp');
+
+
+_______________________________________________________//_______________________________________________________________
+____________________________________________PROCESSAR SASS EM CSS PELO GULP_____________________________________________
+Instalação de Plu-gins Node js...
+
+npm install --save-dev gulp-sass
+
+Pacote Sass
+
+npm install --save-dev sass
+
+Precisamos importar esses pacotes para o arquivo Gulp.js
+
+linha 2
+
+const sass = require('gulp-sass')(require('sass'));
+
+('gulp-sass') ----- responsável por integrar o sass com o Gulp.
+(require('sass')) ----- responsável por fazer toda compilação.
+
+
+FUNÇÂO PARA COMPILAR____________________________________________________________________________
+
+
+//Função para compilar todos arquivos sass em css
+//Usando o * estamos selecionando todos os arquivos SASS >> SCSS << '.source/style/*.scss'.
+//pipe --Vai encadear as funções, sem ele não poderiamos chamar outra função depois do gulp.src
+function compilaSass() {
+    return gulp.src('./source/style/*.scss')
+        .pipe(sass())
+        //Essa função irá mandar toda a compilação para os destino final.
+        .pipe(gulp.dest('./build/styles'));
+    }
+    
+Crie os arquivos scss 
+Passe o comando
+
+npm run gulp sass
+*** os arquivos aparecem compilados na pasta build.
+
--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,54 @@
 
 // Exportar os pacotes Gulp
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
 
+//Função para compilar todos arquivos sass em css
+//Usando o * estamos selecionando todos os arquivos SASS >> SCSS << '.source/style/*.scss'.
+//pipe -- Vai encadear as funções, sem ele não poderiamos chamar outra função depois do gulp.src
+function compilaSass() {
+    return gulp.src('./source/styles/*.scss')
+        .pipe(sass())
+        //Essa função irá mandar toda a compilação para os destino final.
+        .pipe(gulp.dest('./build/styles'));
+    }
+    
+    exports.sass = compilaSass;
+/*
+//Essa tarefa irá rodar 10s depois de todas as outras.
 function funcaoPadrao(callback) {
-    console.log('Executando via Gulp');
-    callback();
+    setTimeout(function() {
+
+        console.log('Primeira tarefa via Gulp');
+        callback();
+    }, 5000) 
 }
 
 function dizOi(callback) {
-    console.log('Hello Word and Gulp');
-    dizTchau();
-    callback();
+    setTimeout(function(){
+        console.log('Hello Word and Gulp');
+        funcaoPadrao();
+        callback();
+
+    },)
 }
 
 function dizTchau(callback) {
-    console.log('Tchau!!!');
-    callback();
+    setTimeout(function() {
+        console.log('Última tarefa, TChauuuuu!!!');
+        callback();
+    },)
 }
 
-exports.default = gulp.series(funcaoPadrao, dizOi);
-exports.dizOi = dizOi
+
+
+//para executar tarefas em SÉRIE:
+//exports.default = gulp.series(funcaoPadrao, dizOi);  
+
+//para executar tarefas em PARALELAMENTE:
+exports.default = gulp.parallel(funcaoPadrao, dizTchau, dizOi); 
+exports.dizOi = dizOi;
+exports.dizTchau = dizTchau;
+//^^^^^^^^^^Observar a sequência de cima^^^^^^^^^^
+*/
